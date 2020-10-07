4cdbd45de7424c02cc83008f8f876cdcc714050d
chamc1984 <chamc1984@gmail.com>
2020-10-07 18:26:34 +0900

    spring-graalvm-native お試し。

--- src/main/java/com/example/springcloudfunctiondemo/SpringCloudFunctionDemoApplication.java
+++ src/main/java/com/example/springcloudfunctiondemo/SpringCloudFunctionDemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.springcloudfunctiondemo;
 
-import org.springframework.boot.SpringApplication;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.example.springcloudfunctiondemo.function.ApiGatewayEventProxyIntegrationFunction;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
-@SpringBootApplication
-public class SpringCloudFunctionDemoApplication {
+@SpringBootApplication(proxyBeanMethods = false)
+public class SpringCloudFunctionDemoApplication implements ApplicationContextInitializer<GenericApplicationContext> {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudFunctionDemoApplication.class, args);
+        FunctionalSpringApplication.run(SpringCloudFunctionDemoApplication.class, args);
     }
 
+    @Override
+    public void initialize(GenericApplicationContext context) {
+        context.registerBean(
+                "apiGatewayEventProxyIntegrationFunction",
+                FunctionRegistration.class,
+                () -> new FunctionRegistration<>(
+                        new ApiGatewayEventProxyIntegrationFunction()
+                ).type(
+                        FunctionType.from(APIGatewayProxyRequestEvent.class)
+                                .to(APIGatewayProxyResponseEvent.class)
+                )
+        );
+    }
 }
